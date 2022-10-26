8ba6d2838102c53f59b04a928bf118766fb352e2
Dung Nguyen T. [2] VN.Hanoi <dungpro1572k@gmail.com>
2022-10-26 10:49:44 +0700

    add text_styles, appbutton

--- lib/presentation/pages/not_found_404_error/not_found_404_error_view.dart
+++ lib/presentation/pages/not_found_404_error/not_found_404_error_view.dart
@@ -1,18 +1,40 @@
+import 'package:baseproject/presentation/navigation/app_navigator_provider.dart';
+import 'package:baseproject/presentation/navigation/app_routes.dart';
+import 'package:baseproject/presentation/resources/app_colors.dart';
 import 'package:baseproject/presentation/resources/app_images.dart';
+import 'package:baseproject/presentation/resources/app_text_styles.dart';
+import 'package:baseproject/presentation/widget/app_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class NotFound404ErrorView extends StatelessWidget {
+class NotFound404ErrorView extends ConsumerStatefulWidget {
   const NotFound404ErrorView({super.key});
 
   @override
+  ConsumerState<NotFound404ErrorView> createState() =>
+      _NotFound404ErrorViewState();
+}
+
+class _NotFound404ErrorViewState extends ConsumerState<NotFound404ErrorView> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          _build404Image(),
-          _buildContent(),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _build404Image(),
+            const SizedBox(
+              height: 40,
+            ),
+            _buildContent(context),
+            const SizedBox(
+              height: 40,
+            ),
+            _buildBackButton(context),
+          ],
+        ),
       ),
     );
   }
@@ -20,7 +42,24 @@ class NotFound404ErrorView extends StatelessWidget {
   Widget _build404Image() {
     return Image.asset(AppImages.notFound);
   }
-  Widget _buildContent(){
-    return const Text('Error 404');
+
+  Widget _buildContent(BuildContext context) {
+    return Text(
+      'Error 404',
+      style: AppTextStyles.headingSmall
+          .copyWith(color: context.colors.textPrimary),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  Widget _buildBackButton(BuildContext context) {
+    return AppButton(
+      title: 'Back to Menu',
+      buttonSize: ButtonSize.medium,
+      isExpanded: true,
+      onButtonTap: () {
+        ref.read(appNavigatorProvider).popUntil(routeName: Approutes.home);
+      },
+    );
   }
 }
