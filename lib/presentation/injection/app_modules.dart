f0d3ed11a1371916ed2d177edbe6f9126ea34d84
dungnt1572000 <dungpro1572k@gmail.com>
2022-11-01 22:27:21 +0700

    fixbug

--- lib/presentation/injection/app_modules.dart
+++ lib/presentation/injection/app_modules.dart
@@ -6,7 +6,9 @@ import 'package:baseproject/data/services/network_service/network_action_manager
 import 'package:baseproject/data/services/network_service/not_found_404_interceptor.dart';
 import 'package:baseproject/data/services/preference_services/shared_preferences_manager.dart';
 import 'package:baseproject/presentation/domain/use_case/get_app_theme_use_case.dart';
+import 'package:baseproject/presentation/domain/use_case/get_driving_direction_object_use_case.dart';
 import 'package:baseproject/presentation/domain/use_case/get_searching_object_use_case.dart';
+import 'package:baseproject/presentation/domain/use_case/get_walking_direction_object_use_case.dart';
 import 'package:baseproject/presentation/injection/injector.dart';
 import 'package:dio/dio.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -44,6 +46,12 @@ class AppModules {
     );
     injector.registerLazySingleton<GetSearchingObjectUseCase>(
       () => GetSearchingObjectUseCase(),
+    );
+    injector.registerLazySingleton<GetDrivingDirectionObjectUseCase>(
+      () => GetDrivingDirectionObjectUseCase(),
+    );
+    injector.registerLazySingleton<GetWalkingDirectionObjectUseCase>(
+      () => GetWalkingDirectionObjectUseCase(),
     );
   }
 }
--- lib/presentation/resources/app_images.dart
+++ lib/presentation/resources/app_images.dart
@@ -1,7 +1,7 @@
 class AppImages{
   AppImages._private();
 
-  static const String _path = "assets/images/";
+  static const String _path = "assets/images";
 
   static const String notFound = "$_path/404.png";
 }
