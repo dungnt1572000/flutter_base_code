65633fc204cf05a94386ee10ee888b5de99d09e6
Dung Nguyen T. [2] VN.Hanoi <103553089+mvn-dungnguyen-t@users.noreply.github.com>
2023-05-31 15:04:18 +0700

    Revert "add speech to text"
    This reverts commit 6e0546d2cca6e1e501316550045dbfbbeac58856.

--- lib/ultilities/flutter_tts.dart
+++ lib/ultilities/flutter_tts.dart
@@ -1,23 +1,23 @@
 import 'package:flutter_tts/flutter_tts.dart';
 
-class FlutterTextToSpeech {
-  FlutterTts flutterTts = FlutterTts();
+class FlutterTextToSpeech{
+   FlutterTts flutterTts = FlutterTts();
+   Future<void> initial()async {
+     await flutterTts.setLanguage("en-US");
 
-  Future<void> initial() async {
-    await flutterTts.setLanguage("en-US");
+     await flutterTts.setSpeechRate(0.5);
 
-    await flutterTts.setSpeechRate(0.34);
+     await flutterTts.setVolume(1.0);
 
-    await flutterTts.setVolume(0.9);
+     await flutterTts.setPitch(1.0);
+   }
+   Future speak(String msg)async{
+     await flutterTts.setSpeechRate(0.5);
+     await flutterTts.speak(msg);
+   }
 
-    await flutterTts.setPitch(1.0);
-  }
-
-  Future speak(String msg) async {
-    await flutterTts.speak(msg);
-  }
-
-  Future stop() async {
+   Future stop() async{
     await flutterTts.stop();
-  }
+   }
+   
 }
