711e7c3084045d8f9b3b160aed2c458050ad5330
dungnt1572000 <dungpro1572k@gmail.com>
2023-07-12 20:07:23 -0700

    change car icon

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,15 @@ import 'package:baseproject/presentation/navigation/app_navigator_provider.dart'
 import 'package:baseproject/presentation/navigation/app_router.dart';
 import 'package:baseproject/presentation/navigation/app_routes.dart';
 import 'package:baseproject/presentation/resources/app_theme.dart';
-import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:baseproject/presentation/injection/injector.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-
-List<CameraDescription> cameras = <CameraDescription>[];
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await AppModules.inject();
   await injector.allReady();
-  cameras = await availableCameras();
   injector
       .isReady<SharedPreferences>()
       .then((value) => runApp(const ProviderScope(
